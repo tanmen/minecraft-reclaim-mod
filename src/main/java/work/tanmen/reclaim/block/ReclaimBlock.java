00f672353cdffa810a40d064b4f0d58ee0b8343a
tanmen <yt.prog@gmail.com>
2021-11-23 07:27:58 +0900

    add attribute for glass

--- src/main/java/work/tanmen/reclaim/block/ReclaimBlock.java
+++ src/main/java/work/tanmen/reclaim/block/ReclaimBlock.java
@@ -1,12 +1,19 @@
 package work.tanmen.reclaim.block;
 
-import net.minecraft.world.level.block.Block;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.block.AbstractGlassBlock;
+import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.material.Material;
 
 
-public class ReclaimBlock extends Block {
+public class ReclaimBlock extends AbstractGlassBlock {
     public ReclaimBlock() {
-        super(BlockBehaviour.Properties.of(Material.STONE).strength(1f));
+        super(BlockBehaviour.Properties.of(Material.GLASS).strength(1f)
+                .sound(SoundType.GLASS)
+                .noOcclusion()
+                .isViewBlocking((BlockState state, BlockGetter getter, BlockPos pos) -> false));
     }
 }
--- src/main/java/work/tanmen/reclaim/block/ReclaimBlocks.java
+++ src/main/java/work/tanmen/reclaim/block/ReclaimBlocks.java
@@ -6,12 +6,7 @@ import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.SaplingBlock;
-import net.minecraft.world.level.block.state.BlockBehaviour;
-import net.minecraft.world.level.material.Material;
-import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fmllegacy.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
@@ -28,7 +23,7 @@ public class ReclaimBlocks {
     public static final RegistryObject<Block> RECLAIM_BLOCK =
             registerBlock("reclaim_block", ReclaimBlock::new);
 
-    private static <T extends Block>RegistryObject<T> registerBlock(String name, Supplier<T> block) {
+    private static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block) {
         RegistryObject<T> registeredBlock = BLOCKS.register(name, block);
         registerBlockItem(name, registeredBlock);
         return registeredBlock;
